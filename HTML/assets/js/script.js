21bab11b329f225ce9ee2c3ed4a90081bd9d248e
tyagi-dhruv <dhruv.mihul@gmail.com>
2025-01-02 22:58:56 +0530

    deleted : HTML

--- HTML/assets/js/script.js
+++
@@ -1,7 +0,0 @@
-// index.js
-document.getElementById("scrollButton").addEventListener("click", function () {
-  const nextPage = document.getElementById("page2");
-  nextPage.scrollIntoView({ behavior: "smooth" });
-});
-
-// person/list.js
